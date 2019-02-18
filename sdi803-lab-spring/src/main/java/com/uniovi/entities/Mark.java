fb6683d590c331df4038ff0cec0acb517518e35c
Ángel García Menéndez <UO258654@uniovi.es>
2019-02-18 14:54:56 +0100

    SDI-SpringBoot- 4 -Acceso a datos con entidades relacionadas

--- sdi803-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi803-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -3,6 +3,8 @@ package com.uniovi.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Mark {
@@ -11,6 +13,9 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
 	public Mark() {
 
@@ -21,6 +26,13 @@ public class Mark {
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 
 	public Long getId() {
@@ -45,6 +57,14 @@ public class Mark {
 
 	public void setScore(Double score) {
 		this.score = score;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	@Override
