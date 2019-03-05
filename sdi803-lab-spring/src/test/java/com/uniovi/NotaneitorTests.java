16b2412ed5a3d95fcb49ee7952c46a6568013370
Ángel García Menéndez <angelgmsi15@gmail.com>
2019-03-05 11:45:03 +0100

    SDI-Selenium-5.1-ClasePrincipal

---
+++ sdi803-lab-spring/src/test/java/com/uniovi/NotaneitorTests.java
@@ -0,0 +1,48 @@
+package com.uniovi;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class NotaneitorTests {
+	private static String PathFirefox65 = "/usr/bin/firefox";
+	private static String Geckdriver024 = "/usr/bin/geckodriver";
+
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "http://localhost:8090";
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		driver.navigate().to(URL);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.manage().deleteAllCookies();
+	}
+
+	@BeforeClass
+	static public void begin() {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
